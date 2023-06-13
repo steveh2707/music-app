2f92bdb2879193a7d19f3526fa9a07ffb2f9a467
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-06-13 14:45:02 +0100

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Steve on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
